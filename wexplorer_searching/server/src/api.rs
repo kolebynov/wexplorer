290fe0746d01befd7540b75f0ff628ec27b5118a
Kaliabianau, Vitali <kolebynov1996@yandex.ru>
2023-09-13 19:43:18 +0300

    feat(Searching): Connect OpenSearch to the searching service

--- wexplorer_searching/server/src/api.rs
+++ wexplorer_searching/server/src/api.rs
@@ -1,6 +1,8 @@
 use std::sync::Mutex;
 
 use num::clamp;
+use opensearch::{OpenSearch, http::transport::Transport, IndexParts};
+use serde_json::json;
 use tonic::{Request, Response, Status};
 use tracing::info;
 
@@ -8,18 +10,13 @@ use self::{searching_api_server::SearchingApi, search_response::result::FoundEnt
 
 tonic::include_proto! {"searching"}
 
-struct PageData {
-    url: String,
-    text: String,
-}
-
 pub struct SearchingApiImpl {
-    storage: Mutex<Vec<PageData>>,
+    open_search_client: OpenSearch,
 }
 
 impl SearchingApiImpl {
     pub fn new() -> Self {
-        Self { storage: Mutex::new(vec![]) }
+        Self { open_search_client: OpenSearch::new(Transport::single_node("http://127.0.0.1:9200").unwrap()) }
     }
 }
 
@@ -27,49 +24,22 @@ impl SearchingApiImpl {
 impl SearchingApi for SearchingApiImpl {
     async fn add_page(&self, request: Request<AddPageRequest>) -> Result<Response<()>, Status> {
         let request = request.into_inner();
-        let mut storage = self.storage.lock().unwrap();
-        if let Some(page_data) = storage.iter_mut().find(|p| p.url == request.url) {
-            info!("Updating text for url {}", request.url);
-            page_data.text = request.text;
-        }
-        else {
-            info!("Adding page to the storage {}", request.url);
-            storage.push(PageData { url: request.url, text: request.text });
-        }
-
+        let body = json!({
+            "url": request.url,
+            "text": request.text,
+        });
+        self.open_search_client
+            .index(IndexParts::Index("search_index"))
+            .body(body)
+            .send()
+            .await
+            .map_err(|err| {
+                Status::internal(err.to_string())
+            })?;
         Ok(Response::new(()))
     }
 
     async fn search(&self, request: Request<SearchRequest>) -> Result<Response<SearchResponse>, Status> {
-        let request = request.into_inner();
-        let results = self.storage.lock().unwrap()
-            .iter()
-            .filter_map(|p| {
-                let mut entries = vec![];
-                let mut index: i32 = 0;
-                loop {
-                    let slice = &p.text[index as usize..];
-                    if let Some(found_index) = slice.find(&request.text) {
-                        index += found_index as i32;
-                        const OFFSET: i32 = 30;
-                        let text_start = p.text.floor_char_boundary(clamp(index - OFFSET, 0, p.text.len() as i32) as usize) as i32;
-                        let text_end = p.text.ceil_char_boundary(clamp(index as usize + request.text.len() + OFFSET as usize, 0, p.text.len()));
-                        entries.push(FoundEntry {
-                            text: p.text[text_start as usize..text_end].to_string(),
-                            highlight_start: index - text_start,
-                            highlight_end: index - text_start + request.text.len() as i32,
-                        });
-                        index += request.text.len() as i32;
-                    }
-                    else {
-                        break;
-                    }
-                }
-
-                if entries.is_empty() { None } else { Some(search_response::Result { url: p.url.clone(), entries }) }
-            })
-            .collect();
-
-        Ok(Response::new(SearchResponse { results }))
+        Ok(Response::new(SearchResponse { ..Default::default() }))
     }
 }
